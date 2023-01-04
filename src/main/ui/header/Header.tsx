fc8dffd9e4b29f14c7e3943059cdf9977df08855
Pavel <3935371@gmail.com>
2023-01-04 18:22:00 +0300

    header h1 tag

--- src/main/ui/header/Header.tsx
+++ src/main/ui/header/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import {PATH} from "../routes/routes";
 
 export const Header = () => {
     return (
         <header>
-            Header
+           <h1>Header</h1>
+           <Link to={PATH.TEST_PAGE}> TestPage </Link>
         </header>
     );
 };
