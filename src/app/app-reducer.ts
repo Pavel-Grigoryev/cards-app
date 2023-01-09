384abe8b672162b108a008b181c642027489fc48
Graf-12 <11skyrus96@mail.ru>
2023-01-09 19:35:12 +0300

    add setAppErrorAC and setAppStatusAC

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,15 +1,35 @@
-const initialState = {}
+const initialState: InitialAppStateType = {
+  status: 'idle',
+  error: null,
+}
 
 export const appReducer = (
   state: InitialAppStateType = initialState,
-  action: any
+  action: ActionsType
 ): InitialAppStateType => {
   switch (action.type) {
+    case 'APP/SET-STATUS':
+      return { ...state, status: action.status }
+    case 'APP/SET-ERROR':
+      return { ...state, error: action.error }
     default:
       return state
   }
 }
 
-//Types
+export const setAppErrorAC = (error: string | null) => ({ type: 'APP/SET-ERROR', error } as const)
+export const setAppStatusAC = (status: RequestStatusType) =>
+  ({ type: 'APP/SET-STATUS', status } as const)
 
-export type InitialAppStateType = typeof initialState
+//Types
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+export type InitialAppStateType = {
+  status: RequestStatusType
+  error: string | null
+}
+
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+
+type ActionsType = SetAppErrorActionType | SetAppStatusActionType
