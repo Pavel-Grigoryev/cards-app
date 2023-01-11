12d7c58322be3203d691ca639300ff5120996074
Graf-12 <11skyrus96@mail.ru>
2023-01-11 21:53:33 +0300

    add Super Button and Super Inputs components

---
+++ src/common/components/SuperButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { Button } from '@mui/material'
+
+type SuperButtonType = {
+  title: string
+  onClick?: () => void
+  href?: string
+}
+
+export const SuperButton = (props: SuperButtonType) => {
+  return (
+    <Button
+      href={props.href}
+      onClick={props.onClick}
+      type={'submit'}
+      variant={'contained'}
+      color={'secondary'}
+      fullWidth
+      style={{
+        fontSize: '16px',
+        textTransform: 'capitalize',
+        borderRadius: '9999px',
+        padding: '4px 16px',
+        width: '80%',
+      }}
+    >
+      {props.title}
+    </Button>
+  )
+}
