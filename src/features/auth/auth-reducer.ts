d00d52c05e7fdf0f2b6ff0751b4560ac8f420223
Graf-12 <11skyrus96@mail.ru>
2023-01-11 12:56:38 +0300

    add password recovery logic

--- src/features/auth/auth-reducer.ts
+++ src/features/auth/auth-reducer.ts
@@ -1,13 +1,15 @@
 import { AxiosError } from 'axios'
 import { Dispatch } from 'redux'
 
-import { authAPI, AuthType } from '../../app/api'
+import { authAPI, AuthType, ForgotType } from '../../app/api'
 import { setAppErrorAC, setAppStatusAC } from '../../app/app-reducer'
 import { setProfileAC } from '../profile/profile-reducer'
 
 const initialState = {
   isLoggedIn: false,
   isInitialized: false,
+  checkEmail: false,
+  saveEmail: '',
 }
 
 type InitialStateType = typeof initialState
@@ -17,6 +19,8 @@ type ActionsType =
   | ReturnType<typeof setProfileAC>
   | ReturnType<typeof setAppStatusAC>
   | ReturnType<typeof setAppErrorAC>
+  | ReturnType<typeof checkEmailAC>
+  | ReturnType<typeof saveEmailAC>
 
 export const authReducer = (
   state: InitialStateType = initialState,
@@ -25,12 +29,20 @@ export const authReducer = (
   switch (action.type) {
     case 'LOGIN':
       return { ...state, isLoggedIn: action.value }
+    case 'CHECK-EMAIL':
+      return { ...state, checkEmail: action.value }
+    case 'SAVE-EMAIL':
+      return { ...state, saveEmail: action.value }
     default:
       return state
   }
 }
 // actions
 export const setLoginAC = (value: boolean) => ({ type: 'LOGIN', value } as const)
+
+export const checkEmailAC = (value: boolean) => ({ type: 'CHECK-EMAIL', value } as const)
+
+export const saveEmailAC = (value: string) => ({ type: 'SAVE-EMAIL', value } as const)
 
 //thunks
 export const signInTC = (data: AuthType) => (dispatch: Dispatch<ActionsType>) => {
@@ -57,6 +69,23 @@ export const signUpTC = (data: AuthType) => (dispatch: Dispatch<ActionsType>) =>
       console.log(res)
       //setLoginAC(true)
       //редирект на логин??
+    })
+    .catch((err: AxiosError<{ error: string }>) => {
+      const error = err.response ? err.response.data.error : err.message
+
+      dispatch(setAppStatusAC('failed'))
+      dispatch(setAppErrorAC(error))
+    })
+}
+
+export const passwordRecoveryTC = (data: ForgotType) => (dispatch: Dispatch<ActionsType>) => {
+  dispatch(setAppStatusAC('loading'))
+  dispatch(saveEmailAC(data.email))
+  authAPI
+    .forgot(data)
+    .then(res => {
+      dispatch(setAppStatusAC('succeeded'))
+      dispatch(checkEmailAC(true))
     })
     .catch((err: AxiosError<{ error: string }>) => {
       const error = err.response ? err.response.data.error : err.message
