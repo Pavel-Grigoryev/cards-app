0253e1390b0ecf3560b0e0c1398e749e1b33ef03
Graf-12 <11skyrus96@mail.ru>
2023-01-15 16:43:00 +0300

    add destructuring component

---
+++ src/features/auth/PasswordRecovery/CheckEmail.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+import FormLabel from '@mui/material/FormLabel'
+import Grid from '@mui/material/Grid'
+
+import { useAppDispatch, useAppSelector } from '../../../app/store'
+import sendMessagePic from '../../../assets/images/sendMessage.png'
+import { SuperButton } from '../../../common/components/SuperButton/SuperButton'
+import { checkEmailAC, saveEmailAC } from '../auth-reducer'
+import styles from '../Login/Login.module.scss'
+
+export const CheckEmail = () => {
+  const dispatch = useAppDispatch()
+
+  const saveEmail = useAppSelector(state => state.auth.saveEmail)
+
+  return (
+    <Grid container justifyContent={'center'} alignItems={'center'}>
+      <Grid item justifyContent={'center'}>
+        <div
+          style={{
+            justifyContent: 'space-evenly',
+            marginTop: '30%',
+            padding: '30px 0',
+            minHeight: '408px',
+          }}
+          className={styles.loginContainer}
+        >
+          <FormLabel>
+            <h1 style={{ color: '#000000', marginBottom: '20px' }}>Check Email</h1>
+          </FormLabel>
+
+          <img
+            style={{ width: '150px', height: '150px', marginBottom: '20px' }}
+            src={sendMessagePic}
+            alt={'Send message pic'}
+          />
+
+          <div style={{ marginBottom: '20px' }}>
+            We’ve sent an Email with instructions to {saveEmail}
+          </div>
+          <SuperButton
+            href={'#/Login'}
+            title={'Back to Login'}
+            onClick={() => {
+              dispatch(checkEmailAC(false))
+              dispatch(saveEmailAC(''))
+            }}
+          />
+        </div>
+      </Grid>
+    </Grid>
+  )
+}
