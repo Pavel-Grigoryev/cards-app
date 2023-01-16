1d84f77741cd2b9f573e07a3d09ab4af0e076fb8
Margarita P <rprivalko@gmail.com>
2023-01-16 12:43:40 +0300

    add(feat): pack page and packs list page

---
+++ src/features/PackPage/PackPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const PackPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Name pack</h1>
+        {/*  Кнопка Learn new pack или add new card (если пак свой)  */}
+      </div>
+      <div>
+        {/* Если пак пустой - кнопка Add new card */}
+        {/* ELSE */}
+        {/*  Компонента для поиска */}
+        {/*  Компонента для таблицы */}
+      </div>
+      {/*  Пагинация */}
+    </>
+  )
+}
