0bfc403bc9e8bb9b9693d778287df3f2c9bc719a
Graf-12 <11skyrus96@mail.ru>
2023-01-19 18:11:51 +0300

    add Reset filters button

--- src/common/components/Filters/Filters.tsx
+++ src/common/components/Filters/Filters.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
+import FilterAltOffIcon from '@mui/icons-material/FilterAltOff'
+import IconButton from '@mui/material/IconButton'
+
+import { useAppDispatch } from '../../../app/store'
+import { setSort } from '../../../features/PacksList/packsList-reducer'
 import { Search } from '../Search/Search'
 
 import s from './Filters.module.scss'
 
 export const Filters = () => {
+  const dispatch = useAppDispatch()
+
   return (
     <div className={s.filterCont}>
       <Search />
+      <IconButton onClick={() => dispatch(setSort({ sortPacks: '' }))} title={'Reset all filters'}>
+        <FilterAltOffIcon fontSize={'large'} />
+      </IconButton>
     </div>
   )
 }
