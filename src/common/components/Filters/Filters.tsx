94941fe8dc25ce73246a6a5e970820cdb6ed9e4a
Graf-12 <11skyrus96@mail.ru>
2023-01-20 11:47:49 +0300

    add ResetFilters component

--- src/common/components/Filters/Filters.tsx
+++ src/common/components/Filters/Filters.tsx
@@ -1,35 +1,18 @@
 import React from 'react'
 
-import FilterAltOffIcon from '@mui/icons-material/FilterAltOff'
-import IconButton from '@mui/material/IconButton'
-
-import { useAppDispatch } from '../../../app/store'
-import { resetFilters } from '../../../features/PacksList/packsList-reducer'
 import { SearchPaperSX } from '../../styles/sx/sx_styles'
+import { ResetFilters } from '../ResetFilters/ResetFilters'
 import { Search } from '../Search/Search'
 
 import s from './Filters.module.scss'
 
-export const defaultFilterValues = {
-  page: 1,
-  sortPacks: '',
-  pageCount: 8,
-  search: '',
-} as const
-
 export const Filters = (props: FiltersPropsType) => {
   const { value, onChange } = props
-  const dispatch = useAppDispatch()
 
   return (
     <div className={s.filterCont}>
       <Search value={value} onChange={onChange} paperStyle={SearchPaperSX} />
-      <IconButton
-        onClick={() => dispatch(resetFilters(defaultFilterValues))}
-        title={'Reset all filters'}
-      >
-        <FilterAltOffIcon fontSize={'large'} />
-      </IconButton>
+      <ResetFilters />
     </div>
   )
 }
--- src/features/PacksList/packsList-reducer.ts
+++ src/features/PacksList/packsList-reducer.ts
@@ -10,7 +10,7 @@ import {
 } from '../../app/api'
 import { setAppErrorAC, setAppStatusAC } from '../../app/app-reducer'
 import { AppThunk } from '../../app/store'
-import { defaultFilterValues } from '../../common/components/Filters/Filters'
+import { defaultFilterValues } from '../../common/components/ResetFilters/ResetFilters'
 import { handleServerNetworkError } from '../../common/utils/error-utils'
 
 const initialState = {
